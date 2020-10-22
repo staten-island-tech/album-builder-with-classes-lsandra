d78599c04c3c03fb503477b640f54e8c1d2673fd
lsandra <57500208+lsandra@users.noreply.github.com>
2020-10-22 14:01:59 -0400

    xcvs

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 console.log("connected");
+/*function getPkmn(){
+    fetch('https://pockeapi.co/api/v2/pockemon/mudkip')
+     .then((result) => {
+         return result.json();
+    })
+    .then((data) => {
+        console.log(data);
+    });
+}
+getPkmn();*/
+
+const pkmn = "mewtwo";
+async function getPkmn(){
+    try {
+        const result = await fetch ( 'https://pockeapi.co/api/v2/pockemon/${pkmn}');
+        const data = await result.json();
+        console.log(data);
+    }catch (error){
+        console.log(error);
+        alert ("did you spell that correctly");
+    }
+}
+getPkmn();
